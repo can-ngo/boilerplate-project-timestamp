088b5acc560ac20f36f499158248cd4f7465e1d1
Can Ngo <nncan99@gmail.com>
2024-09-12 04:15:43 +0000

    Finish Timestamp Microservice

--- index.js
+++ index.js
@@ -2,55 +2,50 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+let isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-  console.log(req);
+app.get("/api/:date", (req, res) => {
+  let date = new Date(req.params.date);
+
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  if (isInvalidDate(date)) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
 });
 
-app.get("/api/:date", function (req, res) {
-  let dateParam = req.params.date;
-  let dateUTC;
-  let dateUNIX;
-  
-  if (dateParam.includes("-")) {
-    let [year, month, day] = dateParam.split("-").map(Number);
-    dateUTC = new Date(Date.UTC(year, month-1, day)).toUTCString();
-    dateUNIX = Date.parse(dateUTC);
-  } else {
-    dateUTC = new Date(Number(dateParam)).toUTCString();
-    dateUNIX = dateParam;
-  }
-  
-  let msg = {
-    unix: dateUNIX,
-    utc: dateUTC
-  }
-  
-  res.send(msg)
-  
-})
-
-
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
